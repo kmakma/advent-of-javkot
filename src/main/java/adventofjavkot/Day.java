d2083bde784b1ee4345904b114bba21520180946
kmakma <michaelo1@gmx.de>
2018-12-06 13:14:25 +0100

    day01 done

---
+++ src/main/java/adventofjavkot/Day.java
@@ -0,0 +1,43 @@
+package adventofjavkot;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Day {
+
+    @Nullable
+    List<String> getInputLines() {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        URL dayInputURL = classloader.getResource(getClass().getSimpleName().toLowerCase());
+        if (dayInputURL == null) {
+            return null;
+        }
+        try {
+            File inputFile = new File(dayInputURL.toURI());
+            return Files.readAllLines(inputFile.toPath());
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+
+        }
+        return null;
+    }
+
+    List<Integer> parseInt(List<String> stringList) {
+        List<Integer> intList = new ArrayList<>();
+        for (String s : stringList) {
+            intList.add(Integer.parseInt(s));
+        }
+        return intList;
+    }
+
+    abstract void partOne();
+
+    abstract void partTwo();
+}
---
+++ src/main/java/adventofjavkot/Day01.java
@@ -0,0 +1,53 @@
+package adventofjavkot;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Day01 extends Day {
+
+
+    private final List<Integer> frequencyChanges;
+
+    private Day01() {
+        frequencyChanges = parseInt(getInputLines());
+        partOne();
+        partTwo();
+    }
+
+    public static void main(String[] args) {
+        new Day01();
+    }
+
+    /**
+     * input equals changes in frequency. Answer is resulting frequency after all changes have been applied.
+     */
+    @Override
+    void partOne() {
+        int sum = frequencyChanges.stream().mapToInt(Integer::intValue).sum();
+        System.out.println("=== STAR 1 ===");
+        System.out.println("Frequency after all changes have been applied: " + sum);
+    }
+
+    @Override
+    void partTwo() {
+        int twiceReachedFrequency = findFirstFrequencyDuplicate();
+
+        System.out.println("=== STAR 2 ===");
+        System.out.println("First frequency reached twice: " + twiceReachedFrequency);
+    }
+
+    private int findFirstFrequencyDuplicate() {
+        Set<Integer> reachedFrequencies = new HashSet<>();
+        int frequency = 0;
+        reachedFrequencies.add(frequency);
+        while (true) {
+            for (int frequencyChange : frequencyChanges) {
+                frequency += frequencyChange;
+                if (!reachedFrequencies.add(frequency)) {
+                    return frequency;
+                }
+            }
+        }
+    }
+}
