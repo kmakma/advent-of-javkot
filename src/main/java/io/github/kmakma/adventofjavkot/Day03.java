b557f2cbc9911062c48ea395a70c7f495ca9c040
kmakma <michaelo1@gmx.de>
2018-12-06 16:35:43 +0100

    day03 started

---
+++ src/main/java/io/github/kmakma/adventofjavkot/Day03.java
@@ -0,0 +1,20 @@
+package io.github.kmakma.adventofjavkot;
+
+public class Day03 extends Day {
+
+    public static void main(String[] args) {
+        new Day03().executeParts();
+    }
+
+    @Override
+    void partOne() {
+
+        System.out.println("=== DAY __ - STAR 1 ===");
+    }
+
+    @Override
+    void partTwo() {
+
+        System.out.println("=== DAY __ - STAR 2 ===");
+    }
+}
